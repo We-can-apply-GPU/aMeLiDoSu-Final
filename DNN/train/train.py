3b0e1bd69edea2ebc80b61f4cc683827b4cf4d9a
Daniel Chin <danielthank@gmail.com>
2015-06-26 19:47:02 +0800

    DNN folder

---
+++ DNN/train/train.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+import numpy as np
+import theano
+import theano.tensor as T
+import lasagne
+import time
+import random
+from StringIO import StringIO
+from settings import *
+
+def load_data(fin, data):
+    OK = True
+    lines = []
+    for z in range(BATCH_SIZE):
+        line = fin.readline()
+        if not line:
+            fin = open(data, "r")
+            line = fin.readline()
+            OK = False
+        lines.append(np.loadtxt(StringIO(line), dtype=np.float32))
+
+    lines = np.asarray(lines)
+    Y = lines[:, 0].astype(np.int32)
+    X = lines[:, 1:]
+
+    return X, Y, OK, fin
+
+def create_iter_functions(data, output_layer):
+    X_batch = T.matrix('x')
+    Y_batch = T.ivector('y')
+    trans = T.matrix('trans')
+    transmap = T.ivector('transmap')
+
+    objective = lasagne.objectives.Objective(output_layer, loss_function=lasagne.objectives.categorical_crossentropy)
+
+    all_params = lasagne.layers.get_all_params(output_layer)
+
+    loss_train = objective.get_loss(X_batch, target=Y_batch)
+
+    pred48 = T.argmax(T.dot(lasagne.layers.get_output(output_layer, X_batch, deterministic=True), trans), axis=1)
+    pred1943 = T.argmax(lasagne.layers.get_output(output_layer, X_batch, deterministic=True), axis = 1)
+    accuracy48 = T.mean(T.eq(pred48, transmap[Y_batch]), dtype=theano.config.floatX)
+    accuracy1943 = T.mean(T.eq(pred1943, Y_batch), dtype=theano.config.floatX)
+
+
+    updates = lasagne.updates.rmsprop(loss_train, all_params, LEARNING_RATE)
+
+    iter_train = theano.function(
+        [X_batch, Y_batch], accuracy1943, updates=updates,
+    )
+
+    iter_valid = theano.function(
+        [X_batch, Y_batch], accuracy48,
+        givens={
+            trans: data['trans'],
+            transmap: data['transmap']
+        }
+    )
+
+    return {"train": iter_train, "valid": iter_valid}
+
+def main():
+    data = {}
+    trans, transmap = load_transition();
+    data['trans'] = theano.shared(trans, borrow=True)
+    data['transmap'] = theano.shared(transmap, borrow=True)
+    print("Building model and compile theano...")
+    output_layer = build_model(input_dim = 350, output_dim = 1943)
+    if len(sys.argv) == 2:
+        print("loading model {}/{}".format(MODEL_NAME, sys.argv[1]))
+        fin = open("model/{}/{}".format(MODEL_NAME, sys.argv[1]))
+        import pickle
+        lasagne.layers.set_all_param_values(output_layer, pickle.load(fin))
+    iter_funcs = create_iter_functions(data, output_layer)
+    print("Training")
+    trainin = open("data/train.dat")
+    validin = open("data/valid.dat")
+    epoch = 0
+    while True:
+
+        accu = 0
+        cnt = 0
+        now = time.time()
+        while True:
+            X, Y, OK, trainin = load_data(trainin, "data/train.dat")
+            if not OK:
+                break
+            accu += iter_funcs['train'](X, Y)
+            cnt += 1
+        accu = accu / cnt
+        print("Epoch {} took {:.3f}s\t{:.2f}%(1943)".format(epoch+1, time.time() - now, accu * 100))
+
+        accu = 0
+        cnt = 0
+        now = time.time()
+        while True:
+            X, Y, OK, validin = load_data(validin, "data/valid.dat")
+            if not OK:
+                break
+            accu += iter_funcs['valid'](X, Y)
+            cnt += 1
+        accu = accu / cnt
+        print("Valid {} took {:.3f}s\t{:.2f}%(39)".format(epoch+1, time.time() - now, accu * 100))
+        print()
+
+        epoch += 1
+        import pickle
+        fout = open("model/{}/{}x{:.2f}".format(MODEL_NAME, epoch, accu * 100), "w")
+        pickle.dump(lasagne.layers.get_all_param_values(output_layer), fout)
+        fout.close()
+
+if __name__ == '__main__':
+  main()
